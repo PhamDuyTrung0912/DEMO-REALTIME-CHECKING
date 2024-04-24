63101dc380742594291b7ffe6602b81c7076132f
trung <trungti09121998@gmail.com>
2024-04-24 23:09:18 +0700

    update

--- index.js
+++ index.js
@@ -1,51 +1,70 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const http = require("http");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const http = require("http");
 
-const app = express();
-const server = http.createServer(app);
+// const app = express();
+// const server = http.createServer(app);
 
-const socketIO = require("socket.io");
-const io = socketIO(server, {
-  allowEIO3: true,
-  cors: {
-    origin: true,
-    credentials: true,
-  },
+// const socketIO = require("socket.io");
+// const io = socketIO(server, {
+//   allowEIO3: true,
+//   cors: {
+//     origin: true,
+//     credentials: true,
+//   },
+// });
+
+// const PORT = 3000;
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// let devices = Array.from({ length: 500 }, (_, index) => ({
+//   id: index + 1,
+//   latitude: Math.random() * 180 - 90,
+//   longitude: Math.random() * 360 - 180,
+// }));
+
+// function updateCoordinates() {
+//     const devices = Array.from({ length: 500 }, (_, index) => ({
+//       id: index + 1,
+//       latitude: Math.random() * 180 - 90,
+//       longitude: Math.random() * 360 - 180,
+//     }));
+
+//     io.emit("DEVICES_INFO", devices);
+// }
+
+
+// // WebSocket connection handler
+// io.on("connection", (ws) => {
+//   console.log(`Listener socket`, ws.id);
+
+//   ws.emit("DEVICES_INFO", devices);
+
+//   setInterval(updateCoordinates, 2000);
+// });
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running at http://localhost:${PORT}`);
+// });
+
+
+// index.js
+const express = require('express')
+const app = express()
+const PORT = 4000
+
+
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
 
-const PORT = 3000;
-
-app.use(cors());
-app.use(bodyParser.json());
-
-let devices = Array.from({ length: 500 }, (_, index) => ({
-  id: index + 1,
-  latitude: Math.random() * 180 - 90,
-  longitude: Math.random() * 360 - 180,
-}));
-
-function updateCoordinates() {
-    const devices = Array.from({ length: 500 }, (_, index) => ({
-      id: index + 1,
-      latitude: Math.random() * 180 - 90,
-      longitude: Math.random() * 360 - 180,
-    }));
-
-    io.emit("DEVICES_INFO", devices);
-}
-
-
-// WebSocket connection handler
-io.on("connection", (ws) => {
-  console.log(`Listener socket`, ws.id);
-
-  ws.emit("DEVICES_INFO", devices);
-
-  setInterval(updateCoordinates, 2000);
-});
-
-server.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+// Export the Express API
+module.exports = app
